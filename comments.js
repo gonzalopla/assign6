def8dba140143d2d8169f5569d4efe4f67dca03d
gonzalopla <gonzalo_p@csu.fullerton.edu>
2020-08-01 12:40:58 -0700

    Update comments.js

--- comments.js
+++ comments.js
@@ -2,7 +2,47 @@
   'use strict';
 
   const BUTTON_SELECTOR = '[data-posts="id"]';
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((response)=> response.json())
+.then((json)=>{
+  json.forEach(function(data) {
 
+    let article=document.createElement("article");
+    let post={
+      title: data.title,
+      body: data.body,
+      id:data.id
+    }
+    let contents=`${post.title}
+                  ${post.body}
+                  ${post.id}`
+let node=document.createTextNode(contents);
+article.appendChild(node);
+document.body.appendChild(article);
+  });
+
+})
+
+/*  if(window.attachEvent) {
+    window.attachEvent('onload', callapi);
+} else {
+    if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function(evt) {
+            curronload(evt);
+            callapi(evt);
+        };
+        window.onload = newonload;
+    } else {
+        window.onload = callapi;
+    }
+}
+function callapi(){
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+}*/
   let buttons = document.querySelectorAll(BUTTON_SELECTOR);
 
   buttons.forEach(function (button) {
